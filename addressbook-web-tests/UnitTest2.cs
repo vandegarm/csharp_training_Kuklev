fd11306ef907dc7b904b97a44ba83ab07adf734f
a.kuklev <vandegarm@yandex.ru>
2019-04-29 12:06:38 +0300

    Исправлено название класса

--- addressbook-web-tests/UnitTest2.cs
+++ addressbook-web-tests/UnitTest2.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 namespace addressbook_web_tests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTest
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
